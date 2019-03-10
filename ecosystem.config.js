13b87104f1237e11ae48914e3d3482e49f6342eb
Park9eon <quiet9.noise@gmail.com>
2019-03-11 01:25:18 +0900

    env modifi

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,8 @@ module.exports = {
     apps: [{
         name: 'ninebot',
         script: 'app.js',
-        watch: true,
+        watch: false,
         env: process.env,
+        env_production: process.env
     }],
 };
